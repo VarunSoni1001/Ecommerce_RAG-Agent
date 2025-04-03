26632b0055a88fdc56e05681439c55da83084cf7
Hitesh Parmar <10hiteshparmar@gmail.com>
2025-04-04 00:00:35 +0530

    fixed file not found error

--- Backend/middlewares/uploadImage.js
+++ Backend/middlewares/uploadImage.js
@@ -30,12 +30,17 @@ const productImgResize = async (req, res, next) => {
   if (!req.files) return next();
   await Promise.all(
     req.files.map(async (file) => {
+      const outputPath = path.join(
+        __dirname,
+        "../public/images/products",
+        file.filename
+      );
+
       await sharp(file.path)
         .resize(300, 300)
         .toFormat("jpeg")
         .jpeg({ quality: 90 })
-        .toFile(`public/images/products/${file.filename}`);
-      fs.unlinkSync(`public/images/products/${file.filename}`);
+        .toFile(outputPath);
     })
   );
   next();
