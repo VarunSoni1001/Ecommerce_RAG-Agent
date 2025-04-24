4288ed13f9d9550e2b1d15514ec01eab6235d26e
Varun Soni <varun1001soni@gmail.com>
2025-04-24 16:56:02 +0530

    refactor: code

--- Frontend/src/pages/Home.js
+++ Frontend/src/pages/Home.js
@@ -18,38 +18,35 @@ const Home = () => {
   const [showRAGAgent, setShowRAGAgent] = React.useState(false);
   // const blogState = useSelector((state) => state?.blog?.blog);
   const productState = useSelector((state) => state?.product?.product);
-   const [storeInformation, setstoreInformation] = useState([]);
-    const { storeInfo } = useSelector(
-      (state) => state.store
-    );
-  
-    useEffect(() => {
-      if (storeInfo && storeInfo.length > 0) {
-        setstoreInformation(storeInfo);
-      }
-    }, [storeInfo]);
-  
-    const existingStoreInfo = storeInformation[0] || {
-      about:
-        "this is the store named RAG Ecom. this is an RAG based ecommerce project",
-      privacy:
-        "Not everyone knows how to make a Privacy Policy agreement, especially with CCPA or GDPR or CalOPPA or PIPEDA or Australia's Privacy Act provisions. If you are not a lawyer or someone who is familiar to Privacy Policies, you will be clueless. Some people might even take advantage of you because of this. Some people may even extort money from you. These are some examples that we want to stop from happening to you.  We will help you protect yourself by generating a Privacy Policy.  Our Privacy Policy Generator can help you make sure that your business complies with the law. We are here to help you protect your business, yourself and your customers.  Fill in the blank spaces below and we will create a personalized website Privacy Policy for your business. No account registration required. Simply generate & download a Privacy Policy in seconds!  Small remark when filling in this Privacy Policy generator: Not all parts of this Privacy Policy might be applicable to your website. When there are parts that are not applicable, these can be removed. Optional elements can be selected in step 2. The accuracy of the generated Privacy Policy on this website is not legally binding. Use at your own risk.",
-      returnPolicy:
-        "return can be done by emailing us and can be processed only withing 2 weeks of purchase",
-      contactDetails: { email: "rag@ecom.com", phone: "1234567890" },
-      address: {
-        street: "Delhi",
-        city: "New Delhi",
-        state: "Delhi",
-        pincode: "110001",
-      },
-    };
+  const [storeInformation, setstoreInformation] = useState([]);
+  const { storeInfo } = useSelector((state) => state.store);
+
+  useEffect(() => {
+    if (storeInfo && storeInfo.length > 0) {
+      setstoreInformation(storeInfo);
+    }
+  }, [storeInfo]);
+
+  const existingStoreInfo = storeInformation[0] || {
+    about:
+      "this is the store named RAG Ecom. this is an RAG based ecommerce project",
+    privacy:
+      "Not everyone knows how to make a Privacy Policy agreement, especially with CCPA or GDPR or CalOPPA or PIPEDA or Australia's Privacy Act provisions. If you are not a lawyer or someone who is familiar to Privacy Policies, you will be clueless. Some people might even take advantage of you because of this. Some people may even extort money from you. These are some examples that we want to stop from happening to you.  We will help you protect yourself by generating a Privacy Policy.  Our Privacy Policy Generator can help you make sure that your business complies with the law. We are here to help you protect your business, yourself and your customers.  Fill in the blank spaces below and we will create a personalized website Privacy Policy for your business. No account registration required. Simply generate & download a Privacy Policy in seconds!  Small remark when filling in this Privacy Policy generator: Not all parts of this Privacy Policy might be applicable to your website. When there are parts that are not applicable, these can be removed. Optional elements can be selected in step 2. The accuracy of the generated Privacy Policy on this website is not legally binding. Use at your own risk.",
+    returnPolicy:
+      "return can be done by emailing us and can be processed only withing 2 weeks of purchase",
+    contactDetails: { email: "rag@ecom.com", phone: "1234567890" },
+    address: {
+      street: "Delhi",
+      city: "New Delhi",
+      state: "Delhi",
+      pincode: "110001",
+    },
+  };
 
   const ragData = {
     products: productState,
     store_information: existingStoreInfo,
   };
-
 
   const navigate = useNavigate();
   const dispatch = useDispatch();
@@ -264,7 +261,9 @@ const Home = () => {
                       </div>
                       <div className="product-image">
                         <img
-                          src={item?.images[0]?.url || "https://placehold.co/400"}
+                          src={
+                            item?.images[0]?.url || "https://placehold.co/400"
+                          }
                           //className="img-fluid d"
                           alt="product image"
                           height={"250px"}
@@ -272,7 +271,9 @@ const Home = () => {
                           onClick={() => navigate("/product/" + item?._id)}
                         />
                         <img
-                          src={item?.images[0]?.url || "https://placehold.co/400"}
+                          src={
+                            item?.images[0]?.url || "https://placehold.co/400"
+                          }
                           //className="img-fluid d"
                           alt="product image"
                           height={"250px"}
@@ -436,7 +437,9 @@ const Home = () => {
                       </div>
                       <div className="product-image">
                         <img
-                          src={item?.images[0]?.url || "https://placehold.co/400"}
+                          src={
+                            item?.images[0]?.url || "https://placehold.co/400"
+                          }
                           // className="img-fluid d"
                           alt="product image"
                           height={"250px"}
@@ -444,7 +447,9 @@ const Home = () => {
                           onClick={() => navigate("/product/" + item?._id)}
                         />
                         <img
-                          src={item?.images[0]?.url || "https://placehold.co/400"}
+                          src={
+                            item?.images[0]?.url || "https://placehold.co/400"
+                          }
                           // className="img-fluid d"
                           alt="product image"
                           height={"250px"}
